9fc10dc16f68ff64fc795e3ac37b1e2a58dcd949
Phuc Tran <thienphucrt@gmail.com>
2023-07-29 14:49:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -160,7 +160,35 @@ app.get("/success", (req, res) => {
         console.log("Get Payment Response");
         console.log(JSON.stringify(payment));
         console.log(payment.payer.status);
-        app.get("/products", getAllProducts);
+        res.send("<html><head><meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+        <body
+          style="
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+          "
+        >
+          <button
+          onclick="sendDataToReactNativeApp()"
+            style="
+              padding: 20;
+              width: 200;
+              font-size: 20;
+              color: white;
+              background-color: #6751ff;
+            "
+          >
+            Send Data To React Native App
+          </button>
+          <script>
+            const sendDataToReactNativeApp = async () => {
+              window.ReactNativeWebView.postMessage('Data from WebView / Website');
+            };
+          </script>
+        </body>
+      </html>");
       }
     }
   );
